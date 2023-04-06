ab9df3942012c11d7828a4b1c5c182a7180b1be2
VinhTran <vinhtrann101@gmail.com>
2023-04-06 14:19:31 -0400

    new colors

--- sketch.js
+++ sketch.js
@@ -116,10 +116,10 @@ function setup() {
 }
 
 function draw() {
-  background('#b1d1fc');
+  background('#4B0092');
   for (let i = 0; i < numSegments - 1; i++) {
     line(xCor[i], yCor[i], xCor[i + 1], yCor[i + 1]);
-    stroke('#c1f80a');
+    stroke('#1AFF1A');
   }
   updateSnakeCoordinates();
   updateHandContainer();
